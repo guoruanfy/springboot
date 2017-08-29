e3f98151838d2a1e9df7fdb77e338af7ac7a8ed8
U-alex-PC\alex <guoruanfy2009@gmail.com>
2017-08-29 23:24:35 +0800

    init project

---
+++ src/main/java/com/alex/demo/DemoBiz.java
@@ -0,0 +1,13 @@
+package com.alex.demo;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by alex on 2017/8/29.
+ */
+@Service
+public class DemoBiz {
+    public String display() {
+        return "sssss";
+    }
+}
